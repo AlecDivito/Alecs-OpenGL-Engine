fec097d33a4f6f138022197f0de55268f38427da
Alec Divito <divito.alec@gmail.com>
2017-02-05 19:51:34 -0500

    Display Window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,60 @@
+// STL
 #include <iostream>
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
 
-using namespace std;
+// Window dimensions
+const Gluint WIDTH = 800, HEIGHT = 600;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    if (!glfwInit())
+    {
+        std::cout << "Failed to Initilize GLFW" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+    // Window hints to use
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Tells GLFW it needs to use opengl 3.3 and up (not allowed to use older functions)
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Tells if window is resizable
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    // Initializes the window
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "MineCraftSandbox", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initializing GLEW (Must be done this way to take a modern approch to initializing)
+    glewExperimental = GL_TRUE; // (if we didn't do this, could cause issues when using the core profile)
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return 1;
+    }
+
+    // Set the size of the rendering window so OpenGL displays the data and coordinates with respect to the window
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    // left, bottom, right, top of the screen coordiantes (we map (-1, 1) to (0, 800) and (0, 600))
+    glViewport(0, 0, width, height);
+
+    while(!glfwWindowShouldClose(window)) // checks if window was instructed to close
+    {
+        glfwPollEvents(); // checks if any events are triggered (like keyboard input or mouse movement events) and calls the corresponding functions
+        glfwSwapBuffers(window); // swap the color buffer (a large buffer that contains color values for each pixel in GLFW's window)
+    }
+
+    glfwTerminate(); // Terminate glfw
     return 0;
 }
