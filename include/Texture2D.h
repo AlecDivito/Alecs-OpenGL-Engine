da01f23b5514db3f63d977a133d703048d2b599e
Alec Divito <divito.alec@gmail.com>
2017-02-07 20:53:23 -0500

    Completed texture class

--- include/Texture2D.h
+++ include/Texture2D.h
@@ -1,15 +1,29 @@
 #ifndef TEXTURE2D_H
 #define TEXTURE2D_H
 
+#include <GL/glew.h>
+#include <SOIL.h>
 
 class Texture2D
 {
     public:
+        // Holds the texture ID
+        GLuint Texture;
+        // Texture formats
+        GLuint Internal_Format;
+        GLuint Image_Format;
+        // Texture configuration
+        GLuint Wrap_S; // Wrapping mode on S axis
+        GLuint Wrap_T; // Wrapping mode on T axis
+        GLuint Filter_Min; // Filtering mode if texture pixels < screen pixels
+        GLuint Filter_Max; // Filtering mode if texture pixels > screen pixels
+        // Constructor (sets default texture modes
         Texture2D();
-        virtual ~Texture2D();
-
+        // Generates texture from image data
+        void Generate(const char* pathToTexture);
+        // Binds the texture as the current active GL_TEXTURE_2D texture object
+        void Bind() const;
     protected:
-
     private:
 };
 
