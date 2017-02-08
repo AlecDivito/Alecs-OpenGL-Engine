8dadd7e8595ffa1e3c238787778a7e4b224f6047
Alec Divito <divito.alec@gmail.com>
2017-02-07 20:36:51 -0500

    Added Texture2D class

---
+++ include/Texture2D.h
@@ -0,0 +1,16 @@
+#ifndef TEXTURE2D_H
+#define TEXTURE2D_H
+
+
+class Texture2D
+{
+    public:
+        Texture2D();
+        virtual ~Texture2D();
+
+    protected:
+
+    private:
+};
+
+#endif // TEXTURE2D_H
---
+++ src/Texture2D.cpp
@@ -0,0 +1,11 @@
+#include "Texture2D.h"
+
+Texture2D::Texture2D()
+{
+    //ctor
+}
+
+Texture2D::~Texture2D()
+{
+    //dtor
+}
