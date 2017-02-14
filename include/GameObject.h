7b78f78cbc6b1ea7924c3680eb959496392087b5
Alec Divito <divito.alec@gmail.com>
2017-02-13 21:39:03 -0500

    Added GameObject super class (but will be treated as a interface)

--- include/GameObject.h
+++ include/GameObject.h
@@ -12,8 +12,23 @@
 class GameObject
 {
     public:
+        // Object state
+        glm::vec3 Position, Size, Velocity;
+        glm::vec3 Color;
+        GLfloat Rotation;
+        GLboolean IsSolid;
+        GLboolean Destroyed;
+        // render state
+        Texture2D sprite;
+        // Constructor(s)
         GameObject();
-        virtual ~GameObject();
+        GameObject(glm::vec3 pos, glm::vec3 size, Texture2D sprite,
+                   glm::vec3 color = glm::vec3(1.0f),
+                   glm::vec3 velocity = glm::vec3(0.0f));
+        // Draw sprite
+        virtual void Draw() = 0;
+        // collision
+        virtual Glboolean CheckCollision(GameObject &obj) = 0;
 
     protected:
 
--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,11 +1,19 @@
 #include "GameObject.h"
 
-GameObject::GameObject()
+GameObject::GameObject() : Position(glm::vec3(0.0f)), Size(glm::vec3(0.0f)),
+                           Velocity(glm::vec3(0.0f)), Color(glm::vec3(1.0f)),
+                           Rotation(0.0f), IsSolid(true), Destroyed(false)
 {
-    //ctor
+
 }
 
-GameObject::~GameObject()
+GameObject::GameObject(glm::vec3 pos, glm::vec3 size, Texture2D sprite,
+                       glm::vec3 color, glm::vec3 velocity) :
+                       Rotation(0.0f), IsSolid(true), Destroyed(false)
 {
-    //dtor
+    this->Position = pos;
+    this->Size = size;
+    this->sprite = sprite;
+    this->color = color;
+    this->velocity = velocity;
 }
